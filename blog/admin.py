d04655b12cd6a0257b7ccfab69a6d7cb933fa8f0
Daksh Lohar <dakshlohar1311@gmail.com>
2025-05-24 14:17:20 +0530

    Initial commit

---
+++ blog/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from blog.models import Blog, Contact, About
+
+# Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    class Media:
+        css = {
+            "all": ("css/main.css",)
+        }
+        
+        js = ("js/blog.js",)
+
+class AboutAdmin(admin.ModelAdmin):
+    class Media:
+        js = ("js/about.js",)
+
+
+
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Contact)
+admin.site.register(About, AboutAdmin)
